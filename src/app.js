bd539120dc3a14d2ecbcc7cc008adb9fbccce40d
gbarska <gbarska@yahoo.com>
2019-06-30 19:37:50 -0300

    refactoring routes

--- src/app.js
+++ src/app.js
@@ -6,36 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//carregar as Rotas
+const indexRoute = require('./routes/index');
+const productsRoute = require('./routes/products');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-const route = router.get('/',(req,res,next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
-
-const create = router.post('/',(req,res,next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id',(req,res,next) => {
-    const id =  req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-});
-
-
-const del = router.delete('/:id',(req,res,next) => {
-    res.status(200).send('OK');
-});
-
-app.use('/',route);
-app.use('/products',create);
-app.use('/products',put);
-app.use('/products',del);
+app.use('/',indexRoute);
+app.use('/products',productsRoute);
 
 module.exports = app;
