1f5a987a7e1b7a582f38cdc7583b2702036961c9
gbarska <gbarska@yahoo.com>
2019-06-30 22:20:26 -0300

    finishing basic CRUD

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,10 +4,16 @@ const express  = require('express');
 const router = express.Router();
 const controller  = require('../controllers/product-controller')
 
+router.get('/', controller.get);
 router.post('/', controller.post);
-
 router.put('/:id', controller.put);
-
 router.delete('/:id', controller.delete);
+router.get('/:slug', controller.getBySlug);
+
+//example how to handle duplicated routes
+router.get('/admin/:id',controller.getById);
+
+//tags
+router.get('/tags/:tag',controller.getByTags);
 
 module.exports = router;
